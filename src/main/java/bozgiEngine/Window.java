eeedb9686432923ae494f75096eae0606850476a
oskar <oskaros040@gmail.com>
2023-04-24 21:57:58 +0200

    Initial commit

---
+++ src/main/java/bozgiEngine/Window.java
@@ -0,0 +1,92 @@
+package bozgiEngine;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+    int width, height;
+    String title;
+    private long glfwWindow;
+
+    private float r, g, b, a;
+
+    private static Window window = null;
+
+    private Window() {
+        this.width = 2560;
+        this.height = 1440;
+        this.title = "Phis";
+        r = 0;
+        g = 0;
+        b = 0;
+        a = 0;
+    }
+
+    public static Window get() {
+        if (Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public void run() {
+        System.out.println("LWJGL Version " + Version.getVersion());
+
+        init();
+        loop();
+
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if (!glfwInit()) {
+            throw new IllegalStateException("Nie można wczyrać GLFW");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Nie można utworzyć ona GLFW");
+        }
+
+        glfwSetCursorPosCallback(glfwWindow, MouseListener::mousePosCallback);
+        glfwSetMouseButtonCallback(glfwWindow, MouseListener::mouseButtonCallback);
+        glfwSetScrollCallback(glfwWindow, MouseListener::mouseScrollCallback);
+        glfwSetKeyCallback(glfwWindow, KeyListener::keyCallback);
+
+        glfwMakeContextCurrent(glfwWindow);
+        glfwSwapInterval(1);
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+    }
+
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            glfwPollEvents();
+
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+
+}
